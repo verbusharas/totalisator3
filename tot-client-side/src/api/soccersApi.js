ce8ddb17b832aa8c37a21571fd3b9709b9880234
Sarunas Verbus <sarunas@verbus.lt>
2021-01-16 21:11:28 +0200

    adds saveFixtures and fetchRegisteredFixtures requests

--- tot-client-side/src/api/soccersApi.js
+++ tot-client-side/src/api/soccersApi.js
@@ -1,3 +1,5 @@
 import HTTP from "./"
 
-export const fetchMatches = (date) => HTTP.get("/fixture?date="+date);
+export const fetchFifaFixtures = (date) => HTTP.get("/fixture/fifa?date=" + date);
+export const saveFixtures = (fixtures) => HTTP.post("/fixture", fixtures);
+export const fetchRegisteredFixtures = (fixtures) => HTTP.get("/fixture", fixtures);
