46a777ff6c3dad8f88243a72ae803b829a9a60bb
Sarunas Verbus <sarunas@verbus.lt>
2021-01-16 21:14:27 +0200

    creates header-content-footer layout

--- tot-client-side/src/App.js
+++ tot-client-side/src/App.js
@@ -1,9 +1,16 @@
+import {BrowserRouter as Router} from "react-router-dom";
+import Header from "./components/Header";
 import Content from "./components/Content/Content";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  return (
-      <Content/>
-  );
+        return (
+            <Router>
+                    <Header/>
+                    <Content/>
+                    <Footer/>
+            </Router>
+        );
 }
 
 export default App;
