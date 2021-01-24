f1c7c557c7f81e26f49ca7f912ccaa127f0a68ac
Sarunas Verbus <sarunas@verbus.lt>
2021-01-24 20:40:50 +0200

    adds custom repository method findMatchesByStatusName

--- tot-server-side/src/main/java/lt/verbus/totalisator/repository/MatchRepository.java
+++ tot-server-side/src/main/java/lt/verbus/totalisator/repository/MatchRepository.java
@@ -4,6 +4,11 @@ import lt.verbus.totalisator.entity.Match;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MatchRepository extends JpaRepository<Match, Long> {
+
+    List<Match> findMatchesByStatusName(String statusName);
+
 }
