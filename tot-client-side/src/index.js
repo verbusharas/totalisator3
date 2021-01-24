e171af1ff0346625c71c0feba3951d8500cc4282
Sarunas Verbus <sarunas@verbus.lt>
2021-01-24 16:30:01 +0200

    adds graphical assets for new UI design

--- tot-client-side/src/index.js
+++ tot-client-side/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import './colors.css';
+import "./style/main.css";
 import App from './App';
 
 ReactDOM.render(
