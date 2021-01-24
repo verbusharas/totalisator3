d729f6a54805d4441378103c9d1510f4e7f8b6ff
Sarunas Verbus <sarunas@verbus.lt>
2021-01-24 20:48:22 +0200

    changes all field naming to camel-cased

--- tot-client-side/src/components/Toteboards/match.js
+++ tot-client-side/src/components/Toteboards/match.js
@@ -15,18 +15,13 @@ const matches =
                 shortCode: "WSY"
             },
             id: 1348812,
-            status_name: "Finished",
+            statusName: "Notstarted",
             league: {
                 id: 974,
                 name: "A-League",
                 countryName: "Australia"
             },
-            scores: {
-                home_score: "1",
-                away_score: "1",
-                ht_score: "0-0",
-                ft_score: "1-1"
-            }
+            "scores": {}
         },
         {
             date: "2021-01-27 08:10:00",
@@ -43,7 +38,7 @@ const matches =
                 shortCode: "WEU"
             },
             id: 1348815,
-            status_name: "Finished",
+            statusName: "Notstarted",
             winner_team_id: "1101",
             league: {
                 id: 974,
@@ -72,7 +67,7 @@ const matches =
                 shortCode: "STU"
             },
             id: 1350424,
-            status_name: "Finished",
+            statusName: "Finished",
             league: {
                 id: 1005,
                 name: "Tipico Bundesliga",
@@ -87,4 +82,56 @@ const matches =
         }
     ]
 
+
 export default matches;
+
+// matches2 = {
+// {
+//     "date": "2021-02-05 19:00:00",
+//     "homeTeam": {
+//     "id": 9991,
+//         "name": "Verbucci Architects",
+//         "img": "https://pbs.twimg.com/profile_images/3506410963/3451e83c5c603ad57cf7c692c630c060.png",
+//         "shortCode": "VAR"
+// },
+//     "awayTeam": {
+//     "id": 9992,
+//         "name": "Sad Pandas",
+//         "img": "https://is3-ssl.mzstatic.com/image/thumb/Purple123/v4/bb/34/f3/bb34f38d-b9f4-e3b7-907f-c46e293d8ec6/source/256x256bb.jpg",
+//         "shortCode": "SPA"
+// },
+//     "statusName": "Notstarted",
+//     "id": 9999991,
+//     "league": {
+//     "id": 9991,
+//         "name": "Grinder Games",
+//         "countryName": "Lithuania"
+// }
+// },
+// {
+//     "date": "2021-01-01 18:15:00",
+//     "homeTeam": {
+//     "id": 9993,
+//         "name": "Charlie Charlington",
+//         "img": "https://img.icons8.com/cotton/2x/charlie-chaplin--v2.png",
+//         "shortCode": "CCH"
+// },
+//     "awayTeam": {
+//     "id": 9994,
+//         "name": "Raging Racoons",
+//         "img": "https://image.flaticon.com/icons/png/128/2298/2298415.png",
+//         "shortCode": "RAC"
+// },
+//     "homeScore": "1",
+//     "awayScore": "1",
+//     "htScore": "0-0",
+//     "ftScore": "1-1",
+//     "statusName": "Finished",
+//     "id": 9999992,
+//     "league": {
+//     "id": 9992,
+//         "name": "Dreamers League",
+//         "countryName": "Denmark"
+// }
+// }
+// }
