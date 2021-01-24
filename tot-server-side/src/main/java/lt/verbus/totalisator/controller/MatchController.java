5f43aa9226e4e41e1a6a32d25b05f62ca761ce99
Sarunas Verbus <sarunas@verbus.lt>
2021-01-25 00:26:33 +0200

    adds response status CREATED

--- tot-server-side/src/main/java/lt/verbus/totalisator/controller/MatchController.java
+++ tot-server-side/src/main/java/lt/verbus/totalisator/controller/MatchController.java
@@ -2,11 +2,12 @@ package lt.verbus.totalisator.controller;
 
 import lt.verbus.totalisator.entity.Match;
 import lt.verbus.totalisator.service.MatchService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,6 +23,7 @@ public class MatchController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     private Match saveMatch(@RequestBody Match match) {
         return matchService.saveMatch(match);
     }
