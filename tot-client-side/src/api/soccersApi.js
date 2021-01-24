15e166a89180bf52106cdfc1d1f39eedf4e1aa5a
Sarunas Verbus <sarunas@verbus.lt>
2021-01-24 20:49:35 +0200

    adds fetches by matches statuses

--- tot-client-side/src/api/soccersApi.js
+++ tot-client-side/src/api/soccersApi.js
@@ -3,4 +3,6 @@ import HTTP from "./"
 export const fetchFifaFixtures = (date) => HTTP.get("/fixture?date=" + date);
 export const saveAsMatches = (matches) => HTTP.post("/match", matches);
 export const fetchMatches = () => HTTP.get("/match");
+export const fetchManagerPendingMatches = () => HTTP.get("/match/pending");
+export const fetchManagerFinishedMatches = () => HTTP.get("/match/finished");
 export const saveAsMatch = (match) => HTTP.post("/match", match);
