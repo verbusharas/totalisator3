35d60d837f79003ddf208a5e5399ef1da760d818
Sarunas Verbus <sarunas@verbus.lt>
2021-01-29 00:50:46 +0200

    turns security back on

--- tot-server-side/src/main/java/lt/verbus/totalisator/config/SecurityConfig.java
+++ tot-server-side/src/main/java/lt/verbus/totalisator/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/**").permitAll()
+                .antMatchers("/login").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling()
